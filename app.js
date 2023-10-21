d52cf583323420658c97f3255fbdc4c3ded0a593
Adriel Fabrício <afabricio@scale.com.br>
2023-10-21 20:59:32 -0300

    feat: implementado escopo base da listagem

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
+const { products } = require('./config/mock');
+
 const port = 8080;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
 // index page
-app.get('/', function(req, res) {
-  res.render('pages/index');
+app.get('/', (req, res) => {
+  res.render('pages/index', {
+    products: products
+  });
 });
 
-// about page
-app.get('/about', function(req, res) {
+// create product
+app.post('/products', (req, res) => {
+  // ...
+});
+
+// product details
+app.get('/products/:id', (req, res) => {
   // ...
 });
 
---
+++ config/mock.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: 1,
+    name: 'T-shirt Python',
+    description: 'Uma camiseta confortável e estilosa com o logotipo do Python.',
+    price: 20,
+    category: 'Acessórios',
+    status: 'Em estoque',
+  },
+  {
+    id: 2,
+    name: 'Caneca Python',
+    description: 'Uma caneca de cerâmica com o logotipo do Python.',
+		price: 20,
+    category: 'Acessórios',
+    status: 'Em estoque',
+	}
+];
+
+module.exports = {
+	products,
+}
